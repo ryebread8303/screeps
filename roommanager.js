f14485560e9a4b4e83d24c4457f2baa945ad3243
O Hedrick <ryebread8403@gmail.com>
2021-03-12 21:33:48 -0600

    roommanager now collects max harvesters per source

--- roommanager.js
+++ roommanager.js
@@ -3,20 +3,29 @@ var roomManager = {
     /** @param {Room} room **/
     run: function(room) {
         const terrain = new Room.Terrain(room.name);
-        var sources = room.find(FIND_SOURCES);
+        if (room.sources == null){
+            var sources = room.find(FIND_SOURCES);
+            room.memory.sources = {};
 
-        for (source of sources) {
-            const pos = source.pos;
-            var harvestPos = 8;
-            var range = [-1,0,1];
-            for (i of range ){
-                for (k of range) {
-                    if (terrain.get(pos.x + i, pos.x + k) = TERRAIN_MASK_WALL) {
-                        harvestPos = harvestPos - 1;
+            
+            for (var source of sources) {
+                const pos = source.pos;
+                const id = source.id;
+                var harvestPos = 8;
+                var record = {};
+                // try to count how many open spots are around a source for harvesting
+                var range = [-1,0,1];
+                for (var i of range ){
+                    for (var k of range) {
+                        if (terrain.get(pos.x + i, pos.x + k) == TERRAIN_MASK_WALL) {
+                            harvestPos = harvestPos - 1;
+                        }
                     }
                 }
+                record.MaxHarvester = harvestPos;
+                record.CurrentHarvester = 0;
+                room.memory.sources[id] = record;
             }
-            room[source.id].availablePos = harvestPos;
         }
     }
 };
