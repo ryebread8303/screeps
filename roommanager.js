3b304ffb21f21fbd796b7aa2bc1dda71da86a25a
O Hedrick <ryebread8403@gmail.com>
2021-03-13 14:42:35 -0600

    changed for loop to increment instead of "of"

--- roommanager.js
+++ roommanager.js
@@ -15,8 +15,8 @@ var roomManager = {
                 var record = {};
                 // try to count how many open spots are around a source for harvesting
                 var range = [-1,0,1];
-                for (var i of range ){
-                    for (var k of range) {
+                for (let i = -1;i < 2;i++){
+                    for (let k = -1;k < 2;k++) {
                         if (terrain.get(pos.x + i, pos.x + k) == TERRAIN_MASK_WALL) {
                             harvestPos = harvestPos - 1;
                         }
